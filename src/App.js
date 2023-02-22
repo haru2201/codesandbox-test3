330ec0d87e5262baaf422838a73ddc2f8b4b328c
haru2201 <97792190+haru2201@users.noreply.github.com>
2023-02-22 14:29:12 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext } from "react";
+
+const SampleContextObject = createContext();
+
+const ConsumerComponent = () => {
+  const messageText = useContext(SampleContextObject);
+
+  console.log(messageText);
+  return <p>{messageText}</p>;
+};
+
+const message = "I love React!!";
+
+export default function App() {
+  return (
+    <SampleContextObject.Provider value={message}>
+      <ConsumerComponent />
+    </SampleContextObject.Provider>
+  );
+}
